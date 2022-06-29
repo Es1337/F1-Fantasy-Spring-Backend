8f729b824ee46e97e7d1a9102ac063cfc18f7337
Michał Kuś <kusmichal06@gmail.com>
2022-06-29 16:57:52 +0200

    Possible Heroku fix

--- src/main/java/zti/project/f1fantasy/F1fantasyApplication.java
+++ src/main/java/zti/project/f1fantasy/F1fantasyApplication.java
@@ -1,16 +1,25 @@
 package zti.project.f1fantasy;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import zti.project.f1fantasy.security.ApplicationSecurityConfig;
 
+import javax.persistence.EntityManager;
+
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class F1fantasyApplication {
+	@Autowired
+	private EntityManager entityManager;
 
 	public static void main(String[] args) {
 //		ApplicationContext context = new AnnotationConfigApplicationContext( ApplicationSecurityConfig.class);
