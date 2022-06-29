abf97fde87f62da3ff54c9a44812092b1cfa5bb5
Michał Kuś <kusmichal06@gmail.com>
2022-06-29 17:34:50 +0200

    Initializing with admin

--- src/main/java/zti/project/f1fantasy/F1fantasyApplication.java
+++ src/main/java/zti/project/f1fantasy/F1fantasyApplication.java
@@ -11,15 +11,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import zti.project.f1fantasy.api.user.User;
+import zti.project.f1fantasy.api.user.UserRepository;
+import zti.project.f1fantasy.api.user.UserService;
 import zti.project.f1fantasy.security.ApplicationSecurityConfig;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @SpringBootApplication
 @PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class F1fantasyApplication {
 	@Autowired
 	private EntityManager entityManager;
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 //		ApplicationContext context = new AnnotationConfigApplicationContext( ApplicationSecurityConfig.class);
@@ -29,6 +35,16 @@ public class F1fantasyApplication {
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	InitializingBean addSuperUser() {
+		System.out.println("add superuser");
+		return () -> {
+			List<User> admin = userRepository.findByEmail("admin");
+			if(admin.size() < 1)
+				userRepository.save(new User("admin", "admin", true));
+		};
 	}
 
 }
