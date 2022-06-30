a412d95bfb160794058cea2d47f274b3ad20adbd
Michał Kuś <kusmichal06@gmail.com>
2022-06-30 17:23:41 +0200

    Trying to fix CORS

--- src/main/java/zti/project/f1fantasy/security/ApplicationSecurityConfig.java
+++ src/main/java/zti/project/f1fantasy/security/ApplicationSecurityConfig.java
@@ -28,6 +28,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
         http.csrf().disable();
 //                .authorizeRequests()
 //                .anyRequest()
